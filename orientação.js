a10a0277d4d2b056d26318e6b23816553de9102d
joao-pedro-de-moura <jpmouraj@gmail.com>
2022-05-11 05:40:30 -0300

    Projeto a ser otimizado

---
+++ orientação.js
@@ -0,0 +1,25 @@
+
+var img = $('#img')
+var vid = $('#vid')
+
+$("#bt").click (function(){
+    var exp = $('#exp')
+    var input = $('#inputData').val()
+    $.ajax({
+        url: `https://api.nasa.gov/planetary/apod?api_key=qQll8YHtQi7NtYVK3hkf4Qy6mYY4pIM45mBJNN7z&date=${input}`,
+        type: "GET", 
+        success: function (req){
+            if (req.media_type == 'image'){
+            img.attr("src", req.url).css("display", "flex")
+            vid.css("display", "none")    
+            }
+            else if(req.media_type == 'video'){
+            vid.attr("src", req.url).css("display", "flex")
+            img.attr("display", "none")   
+            }
+            exp.text(req.explanation)
+        }
+        
+        
+    })
+})
